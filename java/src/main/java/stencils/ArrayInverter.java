9406124e0fe0d24269d619cc9474dbd4b93adfac
Kamil Nguyen Van <nvkga1@gmail.com>
2017-03-01 23:20:09 +0100

    Remove method signature for ArrayInverter

--- java/src/main/java/stencils/ArrayInverter.java
+++ java/src/main/java/stencils/ArrayInverter.java
@@ -2,10 +2,7 @@ package stencils;
 
 
 // the class should implement only one functionality which is inverting of integer arrays
+// example: if (A == [1, 2, 3, 4]) then ( of(A) = [4, 3, 2, 1] )
 public class ArrayInverter {
-	
-	// example: if (A == [1, 2, 3, 4]) then ( of(A) = [4, 3, 2, 1] ) 
-	public static int[] of(int A[]) {
-		return null;
-	}
+
 }
--- java/src/test/java/stencils/ArrayInverterTest.java
+++ java/src/test/java/stencils/ArrayInverterTest.java
@@ -10,15 +10,10 @@ import static org.junit.Assert.assertTrue;
 
 public class ArrayInverterTest {
 
-	int[] A;
-	int[] expected;
-	
     //exemplary test
 	@Test
 	public void exemplaryTest() {
-		A = new int[]{1, 2, 3, 4, 5};
-		expected = new int[]{5, 4, 3, 2, 1};
-		assertTrue(Arrays.equals(expected, ArrayInverter.of(A)));
+		assertTrue(true);
 	}
 
 
