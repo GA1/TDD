ef0918ad0da7b6c582be3450570f25e557936bbb
GA1 <nvkga1@gmail.com>
2017-03-02 16:36:24 +0100

    Finish ArrayInverter

--- java/src/main/java/solved/ArrayInverter.java
+++ java/src/main/java/solved/ArrayInverter.java
@@ -1,14 +1,23 @@
 package solved;
 
-
 // the class should implement only one functionality which is inverting of integer arrays
-// example: if A == [1, 2, 3, 4] then inverted array of A = [4, 3, 2, 1]
+// example: if (A == [1, 2, 3, 4]) then ( of(A) = [4, 3, 2, 1] )
 // an IllegalArgumentException should be thrown if null is provided
 public class ArrayInverter {
 
+    public static int[] revert(int[] numbers) {
+        if (numbers == null)
+            throw  new IllegalArgumentException();
+        for (int i = 0; i < numbers.length / 2; i++)
+            swap(i, numbers.length - 1 - i, numbers);
+        return numbers;
+    }
 
-    public static void revert(Object o) {
-        throw new IllegalArgumentException();
+    private static void swap(int a, int b, int[] numbers) {
+        int temp;
+        temp = numbers[a];
+        numbers[a] = numbers[b];
+        numbers[b] = temp;
     }
 }
 
--- java/src/test/java/solved/ArrayInverterTest.java
+++ java/src/test/java/solved/ArrayInverterTest.java
@@ -1,15 +1,74 @@
 package solved;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 
 public class ArrayInverterTest {
 
-    //exemplary test
 	@Test(expected = IllegalArgumentException.class)
 	public void nullArgument() {
 		ArrayInverter.revert(null);
 	}
 
+	@Test
+	public void emptyArray() {
+		int[] result = ArrayInverter.revert(new int[]{});
+		int[] expected = new int[]{};
+		Assert.assertEquals(expected.length, result.length);
+	}
+
+	@Test
+	public void twoElements() {
+		int[] result = ArrayInverter.revert(new int[]{5, 7});
+		int[] expected = new int[]{7, 5};
+		Assert.assertTrue(areEqual(result, expected));
+	}
+
+	@Test
+	public void threeElements() {
+		int[] result = ArrayInverter.revert(new int[]{5, 7, 11});
+		int[] expected = new int[]{11, 7, 5};
+		Assert.assertTrue(areEqual(result, expected));
+	}
+
+	@Test
+	public void fourElements() {
+		int[] result = ArrayInverter.revert(new int[]{5, 7, 11, 13});
+		int[] expected = new int[]{13, 11, 7, 5};
+		Assert.assertTrue(areEqual(result, expected));
+	}
+
+	@Test
+	public void fiveElements() {
+		int[] result = ArrayInverter.revert(new int[]{5, 7, 11, 13, 17});
+		int[] expected = new int[]{17, 13, 11, 7, 5};
+		Assert.assertTrue(areEqual(result, expected));
+	}
+
+	@Test
+	public void sixElements() {
+		int[] result = ArrayInverter.revert(new int[]{5, 7, 11, 13, 17, 19});
+		int[] expected = new int[]{19, 17, 13, 11, 7, 5};
+		Assert.assertTrue(areEqual(result, expected));
+	}
+
+	@Test
+	public void sevenElements() {
+		int[] result = ArrayInverter.revert(new int[]{5, 7, 11, 13, 17, 19, 21});
+		int[] expected = new int[]{21, 19, 17, 13, 11, 7, 5};
+		Assert.assertTrue(areEqual(result, expected));
+	}
+
+	private static boolean areEqual(int[] array1, int[] array2) {
+		if (array1 == null || array2 == null)
+			return false;
+		if (array1.length != array2.length)
+			return false;
+		for (int i = 0; i < array1.length; i++)
+			if (array2[i] != array1[i])
+				return false;
+		return true;
+	}
 
 }
